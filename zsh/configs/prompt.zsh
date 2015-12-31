7423520f34f378ed382330964bd650afd89ee578
Matthew McClure <m@mmcc.io>
2015-12-31 15:28:51 -0500

    added git dirty check to prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -2,8 +2,22 @@
 git_prompt_info() {
   current_branch=$(git current-branch 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
+    echo " %{$fg_bold[green]%}$current_branch $(parse_git_dirty)%{$reset_color%}"
   fi
 }
+
+# Checks if working tree is dirty, modified from oh-my-zsh.
+function parse_git_dirty() {
+  local STATUS=''
+  local FLAGS
+  FLAGS=('--porcelain')
+  STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+  if [[ -n $STATUS ]]; then
+    echo "$fg_bold[red]✖"
+  else
+    echo "✔"
+  fi
+}
+
 setopt promptsubst
 PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
